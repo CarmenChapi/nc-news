df3d7978411d300691d0e081b7fe4ffa476b3d64
CarmenChapi <mariachaparro58@gmail.com>
2024-09-16 16:54:02 +0100

    add some styling to the display list

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,10 @@ function App() {
 
   return (
     <>
+    <div className='app'>
      <Header/>
      <Body/>
+     </div>
     </>
   )
 }
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,15 +5,19 @@ import ArticleCard from "./ArticleCard";
 const Body = () => {
     const [listArticles, setListArticles] = useState([])
     const [artIdSelected, setArtIdSelected] = useState("")
-    const [isLoading, setIsLoading] = useState(false)
+    const [isLoading, setIsLoading] = useState(true)
     useEffect(()=>{
         getAllArticles().then((articles) => {
            // console.log(articles)
             setListArticles(articles);
-            console.log(listArticles.length)
+            setIsLoading(false)
           });
     }, [])
 
+    if (isLoading) {
+        return <p>...Loading</p>
+      }
+    
 
     return <div><h1>List of Articles</h1>
     <ul className="listArticles-card">
