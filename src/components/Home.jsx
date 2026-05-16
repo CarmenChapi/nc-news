ce6c98d2568ed48935b965a0c7813e9be0d1337b
CarmenChapi <mariachaparro58@gmail.com>
2026-05-16 16:55:43 +0100

    This commit will the logic of pagination

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -39,22 +39,20 @@ const Home= () => {
               setIsLoading(false)
             })
       },[topicByQuery, sortByQuery, orderByQuery, authorByQuery])
- const lastArticleIndex = currentPage * articlesPerPage;
-  const firstArticleIndex = lastArticleIndex - articlesPerPage;
-
-  const currentArticles = listArticles.slice(
-    firstArticleIndex,
-    lastArticleIndex
-  );
-
-  const totalPages = Math.ceil(
-    listArticles.length / articlesPerPage
-  );
 
     
       if (isLoading) {
         return <div><h1 className="loading">...Loading</h1></div>
       }
+      // 2. Lógica matemática para cortar el array
+  const indexOfLastArticle = currentPage * articlesPerPage;
+  const indexOfFirstArticle = indexOfLastArticle - articlesPerPage;
+  
+  // Estos son los 6 artículos que se renderizarán en la página actual
+  const currentArticles = listArticles.slice(indexOfFirstArticle, indexOfLastArticle);
+
+  // Calcular el total de páginas
+  const totalPages = Math.ceil(listArticles.length / articlesPerPage);
     return <section className="listArticles-card">
       {(error && topicByQuery) && <ErrorPage errorMsg={`Wrong value for topic=${topicByQuery}`}/> }
       {error && <ErrorPage errorMsg={error.message}/>}
@@ -67,11 +65,36 @@ const Home= () => {
           <SortBy />
     
     <ul className="listArticles-card">
-        {listArticles.map(article => {
+        {currentArticles.map(article => {
            return <li key={article.article_id}><ArticleCard article={article}/> </li>
             })}
     </ul> 
-    
+    {totalPages > 1 && (
+              <div className="pagination-controls" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '10px', marginTop: '20px' }}>
+                <button className="login-button"
+                  onClick={() => {
+                    setCurrentPage((prev) => Math.max(prev - 1, 1));
+                    window.scrollTo(0, 0);
+                  }}
+                  disabled={currentPage === 1}
+                >
+                  Previous
+                </button>
+                
+                <span>
+                  View {currentPage} de {totalPages}
+                </span>
+
+                <button className="login-button"
+                  onClick={() => {
+                    setCurrentPage((prev) => Math.min(prev + 1, totalPages))
+                       window.scrollTo(0, 0);}}
+                  disabled={currentPage === totalPages}
+                >
+                  Next
+                </button>
+              </div>
+            )}
           
     </div></section>}
     </section>
